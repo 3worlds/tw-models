d2db2df7e79ecec7f65ef312385134235da62093
Ian Davies <ian.davies@anu.edu.au>
2021-04-14 16:41:39 +1000

    setup model library (wip)

---
+++ tw-models/src/fr/cnrs/iees/twmodels/models/ModelsDummy.java
@@ -0,0 +1,6 @@
+package fr.cnrs.iees.twmodels.models;
+//This is here only for package association when loading files from this package. Maybe we should use package string constants.
+
+public class ModelsDummy {
+
+}
---
+++ tw-models/src/fr/cnrs/iees/twmodels/tests/TestsDummy.java
@@ -0,0 +1,6 @@
+package fr.cnrs.iees.twmodels.tests;
+
+//This is here only for package association when loading files from this package. Maybe we should use package string constants.
+public class TestsDummy {
+
+}
