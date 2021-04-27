4f1971fac63431ce93de4f7d870d821525285cbd
Ian Davies <ian.davies@anu.edu.au>
2021-04-27 12:05:58 +1000

    reorganised tw-models

--- tw-models/src/fr/cnrs/iees/twmodels/LibraryTable.java
+++ tw-models/src/fr/cnrs/iees/twmodels/LibraryTable.java
@@ -52,41 +52,36 @@ import fr.cnrs.iees.graph.io.GraphImporter;
  * 
  */
 public enum LibraryTable {
-	/*-	Menu name,	|	ProposedName|	File name,	|	category,	|package file association */
-	Template1("1 Blank", "Prj1", "Blank.utg", LibraryType.Template, TemplatesDummy.class), //
-	Template2("2 SimpleClock", "Prj1", "SimpleClock_1.utg", LibraryType.Template, TemplatesDummy.class), //
+	/*-	Menu name,	|	ProposedName|	File name,	|	category, */
+	Template1("1 Blank", "Prj1", "Blank.utg", LibraryType.Template), //
+	Template2("2 SimpleClock", "Prj1", "SimpleClock_1.utg", LibraryType.Template), //
 	//
-	Tut1("1 Logistic", "Logistic1", "Logistic_1.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut2("2 LotkaVolterra", "LotkaVolterra1", "LotkaVolterra_1.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut3("3 I.D.H.(clock)", "IdhClock1", "IdhClock.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut4("4 I.D.H.(event)", "IdhEvent1", "IdhEvent.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut5("5 Panmixia", "Panmixia1", "Panmixia.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut6("6 Spatial", "Spatial1", "Spatial.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut7("7 Boids", "Boids1", "Flock.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut8("8 LittleForest", "LittleForest1", "LittleForest.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut9("9 Headless(Logistic)", "Headless1", "LogisticHeadless.utg", LibraryType.Tutorial, TutorialsDummy.class), //
-	Tut10("10 Random number generators", "Rng1", "Rng_1.utg", LibraryType.Tutorial, TutorialsDummy.class), //
+	Tut1("1 Logistic", "Logistic1", "Logistic_1.utg", LibraryType.Tutorial), //
+	Tut2("2 LotkaVolterra", "LotkaVolterra1", "LotkaVolterra_1.utg", LibraryType.Tutorial), //
+	Tut3("3 I.D.H.(clock)", "IdhClock1", "IdhClock.utg", LibraryType.Tutorial), //
+	Tut4("4 I.D.H.(event)", "IdhEvent1", "IdhEvent.utg", LibraryType.Tutorial), //
+	Tut5("5 Panmixia", "Panmixia1", "Panmixia.utg", LibraryType.Tutorial), //
+	Tut6("6 Spatial", "Spatial1", "Spatial.utg", LibraryType.Tutorial), //
+	Tut7("7 Boids", "Boids1", "Flock.utg", LibraryType.Tutorial), //
+	Tut8("8 LittleForest", "LittleForest1", "LittleForest.utg", LibraryType.Tutorial), //
+	Tut9("9 Headless(Logistic)", "Headless1", "LogisticHeadless.utg", LibraryType.Tutorial), //
+	Tut10("10 Random number generators", "Rng1", "Rng_1.utg", LibraryType.Tutorial), //
 	//
-	Model1("1 Palms", "Palms1", "Palms.utg", LibraryType.Model, ModelsDummy.class), //
-	Model2("2 Resproutch", "Resproutch1", "Resproutch.utg", LibraryType.Model, ModelsDummy.class), //
+	Model1("1 Palms", "Palms1", "Palms.utg", LibraryType.Model), //
+	Model2("2 Resproutch", "Resproutch1", "Resproutch.utg", LibraryType.Model), //
+	Model3("3 Animal (Continuous Space)", "AnimalC1", "CDU_AnimalC1.utg", LibraryType.Model), //
 	//
-	Test1("1 TestRelations", "TestRelations1", "TestRelations.utg", LibraryType.Test, TestsDummy.class), //
-	Test2("2 TestLifeCycle", "TestLifeCycle1", "TestLifeCycle.utg", LibraryType.Test, TestsDummy.class), //
-	Test3("3 WrapTest", "WrapTest1", "WrapTest.utg", LibraryType.Test, TestsDummy.class), //
-	Test4("4 TestXYPlot", "TestXYPlot1", "TestXYPlot.utg", LibraryType.Test, TestsDummy.class), //
-	Test5("5 ParallelTest (Palms)", "Palms1", "ParallelTestPalms.utg", LibraryType.Test, TestsDummy.class), //
-	Test6("6 ParallelTest (Logistic)", "Logistic1", "ParallelTestLogistic.utg", LibraryType.Test, TestsDummy.class), //
-	Test7("7 ParallelTest (Boids)", "Boids1", "ParallelTestBoids.utg", LibraryType.Test, TestsDummy.class), //
-	Test8("8 ParallelTest (LittleForest)", "LittleForest1", "ParallelTestLittleForest.utg", LibraryType.Test,
-			TestsDummy.class), //
-	Test9("9 Animal (Table)", "AnimalT1", "CDU_AnimalT1.utg", LibraryType.Test, TestsDummy.class), //
-	Test10("10 Animal (Continuous Space)", "AnimalC1", "CDU_AnimalC1.utg", LibraryType.Test, TestsDummy.class), //
-	Test11("11 Timer test (clock/event)", "TimerTest1", "TimerTest.utg", LibraryType.Test, TestsDummy.class), //
-	Test12("12 Timer test (year+offset)", "TimerTest1", "TimerTestOffsetYearClock1.utg", LibraryType.Test,
-			TestsDummy.class), //
-	Test13("13 Timer test (calendar time)", "TimerTest1", "TimerTestCalendarTime.utg", LibraryType.Test,
-			TestsDummy.class), //
-
+	Test1("1 TestRelations", "TestRelations1", "TestRelations.utg", LibraryType.Test), //
+	Test2("2 TestLifeCycle", "TestLifeCycle1", "TestLifeCycle.utg", LibraryType.Test), //
+	Test3("3 WrapTest", "WrapTest1", "WrapTest.utg", LibraryType.Test), //
+	Test4("4 TestXYPlot", "TestXYPlot1", "TestXYPlot.utg", LibraryType.Test), //
+	Test5("5 ParallelTest (Palms)", "Palms1", "ParallelTestPalms.utg", LibraryType.Test), //
+	Test6("6 ParallelTest (Logistic)", "Logistic1", "ParallelTestLogistic.utg", LibraryType.Test), //
+	Test7("7 ParallelTest (Boids)", "Boids1", "ParallelTestBoids.utg", LibraryType.Test), //
+	Test8("8 ParallelTest (LittleForest)", "LittleForest1", "ParallelTestLittleForest.utg", LibraryType.Test), //
+	Test9("9 Timer test (clock/event)", "TimerTest1", "TimerTest.utg", LibraryType.Test), //
+	Test10("10 Timer test (year+offset)", "TimerTest1", "TimerTestOffsetYearClock1.utg", LibraryType.Test), //
+	Test11("11 Timer test (calendar time)", "TimerTest1", "TimerTestCalendarTime.utg", LibraryType.Test), //
 	;
 
 // NB File dependencies e.g SquarePatterns.jav is not implemented yet.
@@ -94,7 +89,6 @@ public enum LibraryTable {
 	private final String proposedName;
 	private final String fileName;
 	private final LibraryType libraryType;
-	private final Class<?> pkclass;
 	/**
 	 * TODO: Just an idea for java, jar and data(?) file dependencies:
 	 * 
@@ -117,13 +111,11 @@ public enum LibraryTable {
 
 	private final String[] depFiles;
 
-	private LibraryTable(String displayName, String proposedName, String fileName, LibraryType lt, Class<?> pkclass,
-			String... depFiles) {
+	private LibraryTable(String displayName, String proposedName, String fileName, LibraryType lt, String... depFiles) {
 		this.displayName = displayName;
 		this.proposedName = proposedName;
 		this.fileName = fileName;
 		this.libraryType = lt;
-		this.pkclass = pkclass;
 		this.depFiles = depFiles;
 	}
 
@@ -145,8 +137,20 @@ public enum LibraryTable {
 
 	@SuppressWarnings("unchecked")
 	public TreeGraph<TreeGraphDataNode, ALEdge> getGraph() {
-		return (TreeGraph<TreeGraphDataNode, ALEdge>) GraphImporter.importGraph(fileName, pkclass);
-
+		switch (libraryType) {
+		case Template: {
+			return (TreeGraph<TreeGraphDataNode, ALEdge>) GraphImporter.importGraph(fileName, TemplatesDummy.class);
+		}
+		case Tutorial: {
+			return (TreeGraph<TreeGraphDataNode, ALEdge>) GraphImporter.importGraph(fileName, TutorialsDummy.class);
+		}
+		case Model: {
+			return (TreeGraph<TreeGraphDataNode, ALEdge>) GraphImporter.importGraph(fileName, ModelsDummy.class);
+		}
+		default: {
+			return (TreeGraph<TreeGraphDataNode, ALEdge>) GraphImporter.importGraph(fileName, TestsDummy.class);
+		}
+		}
 	}
 
 }
